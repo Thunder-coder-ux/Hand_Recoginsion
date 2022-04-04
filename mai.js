9ff34dac19cb7f9b873bda581b2f584be7fa268c
Ninja_joeneys <81759049+Thunder-coder-ux@users.noreply.github.com>
2022-04-04 18:27:09 +0530

    Add files via upload

--- mai.js
+++ mai.js
@@ -23,10 +23,50 @@ console.log("ml5 version", ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/QftZCHD-V/model.json' , modelLoaded);
 
 function modelLoaded(){
-          console.log("Model has been loaded");
+    console.log("Model has been loaded.");
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first guess is"+ prediction_1;
+    speak_data_2 = "And the second guess is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
 }
 
 function check(){
     img = document.getElementById("captured_image");
     classifier.classify(img, gotResult);
 }
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    document.getElementById("result_gesture_name1").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak()
+    if(results[0].label="Victory"){
+    document.getElementById("update_emoji").innerHTML = "&#9996;";
+    }
+    if(results[0].label="Thump up"){
+        document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(results[0].label="Okay"){
+       document.getElementById("update_emoji").innerHTML = "&#128076;";
+   }
+    if(results[1].label="Victory"){
+    document.getElementById("update_emoji1").innerHTML = "&#9996;";
+     }
+    if(results[1].label="Thump up"){
+        document.getElementById("update_emoji1").innerHTML = "&#128077;";
+    }
+    if(results[1].label="Okay"){
+       document.getElementById("update_emoji1").innerHTML = "&#128076;";
+      }
+   }
+   }
